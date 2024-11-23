b0c3536ecbaa1a1c67c7d795c547f5daa88ab2d0
Mehraz71 <73421318+Mehraz71@users.noreply.github.com>
2024-11-24 00:54:28 +0600

    Update taskCdeg.php

--- lab3.1/DEGREE/taskCdeg.php
+++ lab3.1/DEGREE/taskCdeg.php
@@ -1,34 +1,35 @@
 <?php
+$degree = [];
+
 if (isset($_POST['submit'])) {
-    // Check if any degrees were selected
     if (isset($_POST['degree'])) {
-        // Retrieve the selected degrees as an array
-        $degrees = $_POST['degree'];
-        
-        // Display the selected degrees
-        echo "Selected degrees: <br />";
-        foreach ($degrees as $degree) {
-            echo $degree . "<br />";
-        }
-    } else {
-        echo "No degree selected.";
+        $degree = $_POST['degree']; 
+    }
+
+    echo "Selected degrees are: <br>";
+    foreach ($degree as $deg) {
+        echo $deg . "<br>";
     }
 }
 ?>
 
 <html>
   <head>
-    <title>Degree Selection</title>
+    <title>Degree</title>
   </head>
   <body>
     <form method="post">
       <fieldset>
         <legend>Degree</legend>
         Degree : <br />
-        <input type="checkbox" name="degree[]" value="SSC" /> SSC
-        <input type="checkbox" name="degree[]" value="HSC" /> HSC
-        <input type="checkbox" name="degree[]" value="BSC" /> BSC
-        <input type="checkbox" name="degree[]" value="MSC" /> MSC<br />
+        <input type="checkbox" name="degree[]" value="SSC" 
+          <?php if (in_array('SSC', $degree)) echo 'checked'; ?> /> SSC<br />
+        <input type="checkbox" name="degree[]" value="HSC" 
+          <?php if (in_array('HSC', $degree)) echo 'checked'; ?> /> HSC<br />
+        <input type="checkbox" name="degree[]" value="BSC" 
+          <?php if (in_array('BSC', $degree)) echo 'checked'; ?> /> BSc<br />
+          <input type="checkbox" name="degree[]" value="MSC" 
+          <?php if (in_array('MSC', $degree)) echo 'checked'; ?> /> MSc<br />
 
         <input type="submit" name="submit" value="Submit" />
       </fieldset>
